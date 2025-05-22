ec4807f047535424e899fa77980e45d4f0ab1674
PontshoMathobela <pontshomathobs@gmail.com>
2025-05-22 03:54:02 +0200

    Update routing.py

--- multichat/connect/routing.py
+++ multichat/connect/routing.py
@@ -2,6 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/private/$", consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/somepath/$', consumers.YourConsumer.as_asgi()),
 ]
 
